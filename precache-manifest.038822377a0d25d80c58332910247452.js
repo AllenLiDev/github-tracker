d13c5e670fb4d8bc46b4334a64ebc1d987d2b151
Allen Li <headhuntar@gmail.com>
2020-01-20 16:48:36 -0800

    Updates

---
+++ precache-manifest.038822377a0d25d80c58332910247452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97f19dbcdc92e07959e082a0d58789a",
+    "url": "/github-tracker/index.html"
+  },
+  {
+    "revision": "ec9d3cbecf83ec679a3a",
+    "url": "/github-tracker/static/js/2.11aa1f17.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/github-tracker/static/js/2.11aa1f17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7339fb4c95e1e20cf32b",
+    "url": "/github-tracker/static/js/main.ba4af4f5.chunk.js"
+  },
+  {
+    "revision": "a84bee3396b4a6e22ad2",
+    "url": "/github-tracker/static/js/runtime-main.98f58f19.js"
+  }
+]);
